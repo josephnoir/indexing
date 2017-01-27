08cc66a545755bcdd91e153b8c22393d174b88c3
Joseph Noir <joseph.noir@gmail.com>
2017-01-27 17:56:22 +0100

    Clean up find order

--- cmake/FindVAST.cmake
+++ cmake/FindVAST.cmake
@@ -32,35 +32,35 @@ find_path(VAST_INCLUDE_DIR
             /opt/local/include
             /sw/include
             ${CMAKE_INSTALL_PREFIX}/include)
-find_path(VAST_INCLUDE_CONF_DIR
-          NAMES
-            "vast/config.hpp"
-          HINTS
-            ${header_hints}
-            /usr/include
-            /usr/local/include
-            /opt/local/include
-            /sw/include
-            ${CMAKE_INSTALL_PREFIX}/include)
 mark_as_advanced(VAST_INCLUDE_DIR)
-mark_as_advanced(VAST_INCLUDE_CONF_DIR)
 set(VAST_INCLUDE_DIRS ${VAST_INCLUDE_DIRS} ${VAST_INCLUDE_DIR})
-set(VAST_INCLUDE_DIRS ${VAST_INCLUDE_DIRS} ${VAST_INCLUDE_CONF_DIR})
 if (NOT "${VAST_INCLUDE_DIR}" STREQUAL "VAST_INCLUDE_DIR-NOTFOUND")
-  # mark as found (set back to false in case library cannot be found)
-  set(VAST_FOUND true)
-  # add to VAST_INCLUDE_DIRS only if path isn't already set
-  set(duplicate false)
-  foreach (p ${VAST_INCLUDE_DIRS})
-    if (${p} STREQUAL ${VAST_INCLUDE_DIR})
-      set(duplicate true)
-    endif ()
-  endforeach ()
-  if (NOT duplicate)
-    set(VAST_INCLUDE_DIRS ${VAST_INCLUDE_DIRS} ${VAST_INCLUDE_DIR})
-  endif()
-  # look for (.dll|.so|.dylib) file, again giving hints for non-installed VASTs
-  # skip probe_event as it is header only
+  find_path(VAST_INCLUDE_CONF_DIR
+        NAMES
+          "vast/config.hpp"
+        HINTS
+          ${header_hints}
+          /usr/include
+          /usr/local/include
+          /opt/local/include
+          /sw/include
+          ${CMAKE_INSTALL_PREFIX}/include)
+  if (NOT "${VAST_INCLUDE_DIR}" STREQUAL "VAST_INCLUDE_DIR-NOTFOUND")
+    # mark as found (set back to false in case library cannot be found)
+    set(VAST_FOUND true)
+    # add to VAST_INCLUDE_CONF_DIRS only if path isn't already set
+    set(duplicate false)
+    foreach (p ${VAST_INCLUDE_DIRS})
+      if (${p} STREQUAL ${VAST_INCLUDE_CONF_DIR})
+        set(duplicate true)
+      endif ()
+    endforeach ()
+    if (NOT duplicate)
+      set(VAST_INCLUDE_DIRS ${VAST_INCLUDE_DIRS} ${VAST_INCLUDE_CONF_DIR})
+    endif()
+    mark_as_advanced(VAST_INCLUDE_CONF_DIR)
+    # look for (.dll|.so|.dylib) file, again giving hints for non-installed VASTs
+    # skip probe_event as it is header only
     if (VAST_ROOT_DIR)
       set(library_hints "${VAST_ROOT_DIR}/lib")
     endif ()
@@ -80,6 +80,7 @@ if (NOT "${VAST_INCLUDE_DIR}" STREQUAL "VAST_INCLUDE_DIR-NOTFOUND")
     else ()
       set(VAST_LIBRARIES ${VAST_LIBRARIES} ${VAST_LIBRARY})
     endif ()
+  endif ()
 endif ()
 
 # let CMake check whether all requested components have been found
