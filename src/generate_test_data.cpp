08b5deebae63d83786b3fc815096c5f0756331f9
Joseph Noir <joseph.noir@gmail.com>
2017-01-30 13:15:48 +0100

    Add generator for test data (only uint32_t atm.)

---
+++ src/generate_test_data.cpp
@@ -0,0 +1,47 @@
+
+#include <random>
+#include <iostream>
+
+#include "caf/all.hpp"
+
+using namespace std;
+using namespace caf;
+
+class config : public actor_system_config {
+public:
+  size_t from   =      0;
+  size_t to     =   1023;
+  size_t amount = 250000;
+  string separator = "\n";
+
+  config() {
+    opt_group{custom_options_, "global"}
+    .add(from, "from,f", "set minimum value (default: 0)")
+    .add(to,   "to,t", "set maximum value (default: 1023)")
+    .add(amount, "amount,a", "set number of values (default: 250000)")
+    .add(separator, "separator,s", "separator between numbers (default: \\n)");
+  }
+};
+
+int main(int argc, char** argv) {
+  config cfg;
+  // read CLI options
+  cfg.parse(argc, argv);
+  // return immediately if a help text was printed
+  if (cfg.cli_helptext_printed)
+    return 0;
+
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_int_distribution<uint32_t> dist(cfg.from,cfg.to);
+  vector<uint32_t> values(cfg.amount);
+  auto& sep = cfg.separator;
+  for (size_t i = 0; i < cfg.amount; ++i) {
+    cout << dist(rng);
+    if (i < cfg.amount - 1) {
+      cout << sep;
+    }
+  }
+  cout << endl;
+}
+
