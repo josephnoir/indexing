6b36738bd3100faa2231957961d184dd55472909
Joseph Noir <joseph.noir@gmail.com>
2017-01-31 14:52:27 +0100

    Add cli options

--- src/vst.cpp
+++ src/vst.cpp
@@ -3,11 +3,14 @@
 #include <random>
 #include <vector>
 #include <cassert>
+#include <fstream>
 #include <iomanip>
 #include <numeric>
 #include <sstream>
 #include <iostream>
 #include <algorithm>
+
+#include "caf/all.hpp"
 
 #include "vast/coder.hpp"
 #include "vast/operator.hpp"
@@ -18,75 +21,62 @@
 #include "vast/concept/printable/vast/bitmap.hpp"
 
 using namespace std;
+using namespace caf;
 using namespace vast;
 
-namespace {
+class config : public actor_system_config {
+public:
+  string filename = "";
+  uint32_t bound = 0;
+  config() {
+    opt_group{custom_options_, "global"}
+    .add(filename, "data-file,f", "File to read test data from (one value per line)")
+    .add(bound, "bound,b", "maximum value in the set (0 will scan values)");
+  }
+};
 
-template<class T>
-string as_binary(T num) {
-  stringstream s;
-  auto num_bits = (sizeof(T) * 8);
-  auto added_bits = 0;
-  T mask = T(0x1) << (num_bits - 1);
-  while (mask > 0) {
-    if (added_bits == 32) {
-      s << " ";
-      added_bits = 0;
+void caf_main(actor_system&, const config& cfg) {
+  vector<uint32_t> values;
+  if (cfg.filename.empty()) {
+    values = {10,  7, 22,  6,  7,  1,  9, 42,  2,  5,
+              13,  3,  2,  1,  0,  1, 18, 18,  3, 13,
+               5,  9,  0,  3,  2, 19,  5, 23, 22, 10,
+               6, 22};
+  } else {
+    cout << "Reading data from '" << cfg.filename << "' ... " << flush;
+    ifstream source{cfg.filename, std::ios::in};
+    uint32_t next;
+    while (source >> next) {
+      values.push_back(next);
     }
-    s << ((num & mask) ? "1" : "0");
-    mask >>= 1;
-    ++added_bits;
   }
-  return s.str();
-}
+  auto amount = values.size();
+  cout << "Read " << amount << " values." << endl;
+  auto bound = cfg.bound;
+  if (bound == 0 && amount > 0) {
+    auto itr = max_element(values.begin(), values.end());
+    bound = *itr;
+  }
+  cout << "Maximum value is  " << bound << "." << endl;
 
-} // namespace <anonymous>
+  // Extract key set
+  vector<uint32_t> keys = values;
+  std::sort(keys.begin(), keys.end());
+  auto last = std::unique(keys.begin(), keys.end());
+  keys.erase(last, end(keys));
 
-int main(void) {
-  /*
-  vector<uint32_t> values{10,  7, 22,  6,  7,
-                           1,  9, 42,  2,  5,
-                          13,  3,  2,  1,  0,
-                           1, 18, 18,  3, 13,
-                           5,  9,  0,  3,  2,
-                          19,  5, 23, 22, 10,
-                           6, 22};
-  // auto amount = values.size();
-  */
-
-  size_t max_value = 32;
-  size_t amount = 1024;
-  std::mt19937 rng;
-  rng.seed(std::random_device()());
-  std::uniform_int_distribution<uint32_t> dist(0,max_value - 1);
-  vector<uint32_t> values(amount);
-  for (size_t i = 0; i < amount; ++i)
-    values[i] = dist(rng);
-
-  vector<uint32_t> distinct = values;
-  std::sort(distinct.begin(), distinct.end());
-  auto last = std::unique(distinct.begin(), distinct.end());
-  distinct.erase(last, end(distinct));
-  // for (auto val : values) {
-  //   cout << val << " ";
-  // }
-  // cout << endl;
-
-  bitmap_index<uint32_t, equality_coder<wah_bitmap>> bmi{max_value};
-  for (auto& val : values) {
+  // MEASURE: from
+  bitmap_index<uint32_t, equality_coder<wah_bitmap>> bmi{bound + 1};
+  for (auto& val : values)
     bmi.push_back(val);
-  }
-  // for (auto& key : distinct) {
-  //   cout << "Index for value " << key << ":" << endl;
-  //   cout << to_string(bmi.lookup(vast::equal, key)) << endl;
-  // }
+  // MEASURE: to
+
+  // print index by key
   auto& coder = bmi.coder();
   auto& storage = coder.storage();
-  for (auto& key : distinct) {
-    cout << "Index for value " << key << ":" << endl;
-    cout << to_string(storage[key]) << endl;
-  }
-  // for (auto& block : .blocks()) {
-  //   cout << block << endl;
-  // }
+  for (auto& key : keys)
+    cout << "Index for value " << key << ":" << endl
+         << to_string(storage[key]) << endl;
 }
+
+CAF_MAIN()
