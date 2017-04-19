f0993b70145e89f65188eab18e68d52191cf8996
Joseph Noir <joseph.noir@gmail.com>
2017-04-19 16:19:36 +0200

    Add code for block-level segmented scan

---
+++ include/segmented_scan.cl
@@ -0,0 +1,178 @@
+
+// For optimizations regarding bank conflicts, look at:
+// - http://http.developer.nvidia.com/GPUGems3/gpugems3_ch39.html
+// - Scan Primitives for GPUs (Sengupta et al)
+
+/// Global segmented scan, phase 1. From: 
+/// - Scan Primitives for GPUs (Sengupta et al)
+kernel void upsweep(global uint* restrict data,
+                    global uint* restrict heads,
+                    global uint* restrict increments,
+                    global uint* restrict increment_heads,
+                    global uint* restrict tree,
+                    local uint* tmp_data,
+                    local uint* tmp_heads,
+                    uint len) {
+  const uint thread = get_local_id(0);
+  const uint block = get_group_id(0);
+  const uint threads_per_block = get_local_size(0);
+  const uint elements_per_block = threads_per_block * 2;
+  const uint global_offset = block * elements_per_block;
+  const uint n = elements_per_block;
+
+  uint offset = 1;
+  uint even = 2 * thread;
+  uint odd = 2 * thread + 1;
+  tmp_data[even] = (even < len) ? data[global_offset + (even)] : 0;
+  tmp_data[odd] = (odd < len) ? data[global_offset + (odd)] : 0;
+  tmp_heads[even] = (even < len) ? heads[global_offset + (even)] : 0;
+  tmp_heads[odd] = (odd < len) ? heads[global_offset + (odd)] : 0;
+  // build sum in place up the tree
+  for (uint d = n >> 1; d > 0; d >>= 1) {
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (thread < d) {
+      int ai = offset * (even + 1) - 1;
+      int bi = offset * (odd + 1) - 1;
+      // change to segmented scan operator
+      // eval flag ...
+      tmp_data[bi] += tmp_heads[bi] ? 0 : tmp_data[ai];
+      // ... before changing it!
+      tmp_heads[bi] |= tmp_heads[ai];
+    }
+    offset <<= 1; // offset *= 2;
+  }
+  if (thread == 0) {
+    // TODO: modification for segmented scan
+    increments[block] = tmp_data[n - 1];
+    increment_heads[block] = tmp_heads[n - 1];
+    tree[block] = tmp_heads[n - 1];
+    tmp_data[n - 1] = 0;
+  }
+  if (even < len) {
+    data[global_offset + even] = tmp_data[even];
+    heads[global_offset + even] = tmp_heads[even];
+  }
+  if (odd < len) {
+    data[global_offset + odd] = tmp_data[odd];
+    heads[global_offset + odd] = tmp_heads[odd];
+  }
+}
+
+/// Global exclusive scan, phase 2.
+kernel void block_scan(global uint* restrict data,
+                       global uint* restrict heads,
+                       //global uint* restrict tree,
+                       uint len) {
+  local uint tmp_data[2048];
+  local uint tmp_heads[2048];
+  const uint thread = get_local_id(0);
+  const uint n = 2048;
+  uint offset = 1;
+  uint even = 2 * thread;
+  uint odd = 2 * thread + 1;
+  tmp_data[even] = (even < len) ? data[even] : 0;
+  tmp_data[odd] = (odd < len) ? data[odd] : 0;
+  tmp_heads[even] = (even < len) ? heads[even] : 1;
+  tmp_heads[odd] = (odd < len) ? heads[odd] : 1;
+  // build sum in place up the tree
+  for (uint d = n >> 1; d > 0; d >>= 1) {
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (thread < d) {
+      int ai = offset * (even + 1) - 1;
+      int bi = offset * (odd + 1) - 1;
+      // change to segmented scan operator
+      // eval flag ...
+      tmp_data[bi] += tmp_heads[bi] ? 0 : tmp_data[ai];
+      // ... before changing it!
+      tmp_heads[bi] |= tmp_heads[ai];
+    }
+    offset *= 2;
+  }
+  if (thread == 0) {
+    tmp_data[n - 1] = 0;
+  }
+  // traverse down tree & build scan
+  for (uint d = 1; d < n; d <<= 1) {
+    offset >>= 1;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (thread < d) {
+      int ai = offset * (even + 1) - 1;
+      int bi = offset * (odd + 1) - 1;
+      uint next = (offset * (even + 1));
+      uint t = tmp_data[ai];
+      uint old = tmp_data[bi]; // TODO: delete this
+      tmp_data[ai] = tmp_data[bi];
+      // modified for segmented scan
+      if (heads[next] == 1) // what if next >= len
+        tmp_data[bi] = 0;
+      else if (tmp_heads[ai] == 1)
+        tmp_data[bi] = t;
+      else
+        tmp_data[bi] += t;
+      tmp_heads[ai] = 0;
+    }
+  }
+  barrier(CLK_LOCAL_MEM_FENCE);
+  if (even < len) {
+    data[even] = tmp_data[even];
+    heads[even] = tmp_heads[even];
+  }
+  if (odd < len) {
+    data[odd] = tmp_data[odd];
+    heads[odd] = tmp_heads[odd];
+  }
+}
+
+// Global segmented scan phase 3.
+kernel void downsweep(global uint* restrict data,
+                      global uint* restrict heads,
+                      global uint* restrict increments,
+                      global uint* restrict increment_heads,
+                      global uint* restrict tree,
+                      local uint* tmp_data,
+                      local uint* tmp_heads,
+                      uint len) {
+  const uint thread = get_local_id(0);
+  const uint block = get_group_id(0);
+  const uint threads_per_block = get_local_size(0);
+  const uint elements_per_block = threads_per_block * 2;
+  const uint global_offset = block * elements_per_block;
+  const uint n = elements_per_block;
+
+  uint offset = 1;
+  uint even = 2 * thread;
+  uint odd = 2 * thread + 1;
+  tmp_data[even] = (even < len) ? data[global_offset + (even)] : 0;
+  tmp_data[odd] = (odd < len) ? data[global_offset + (odd)] : 0;
+  tmp_heads[even] = (even < len) ? heads[global_offset + (even)] : 0;
+  tmp_heads[odd] = (odd < len) ? heads[global_offset + (odd)] : 0;
+  // traverse down tree & build scan
+  for (uint d = 1; d < n; d *= 2) {
+    offset >>= 1;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (thread < d) {
+      int ai = offset * (even + 1) - 1;
+      int bi = offset * (odd + 1) - 1;
+      uint next = global_offset + (offset * (even + 1));
+      uint t = tmp_data[ai];
+      tmp_data[ai] = tmp_data[bi];
+      // modified for segmented scan
+      if (heads[next] == 1) // what if next >= len
+        tmp_data[bi] = 0;
+      else if (tmp_heads[ai] == 1)
+        tmp_data[bi] = t;
+      else
+        tmp_data[bi] += t;
+      tmp_heads[ai] = 0;
+    }
+  }
+  barrier(CLK_LOCAL_MEM_FENCE);
+  if (even < len) {
+    data[global_offset + even] = tmp_data[even];
+    heads[global_offset + even] = tmp_heads[even];
+  }
+  if (odd < len) {
+    data[global_offset + odd] = tmp_data[odd];
+    heads[global_offset + odd] = tmp_heads[odd];
+  }
+}
---
+++ src/segmented_scan.cpp
@@ -0,0 +1,280 @@
+
+#include <cmath>
+#include <tuple>
+#include <chrono>
+#include <random>
+#include <vector>
+#include <cassert>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <numeric>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+#include <unordered_map>
+
+#include "caf/all.hpp"
+#include "caf/opencl/all.hpp"
+#include "caf/opencl/mem_ref.hpp"
+#include "caf/opencl/actor_facade_phase.hpp"
+
+using namespace std;
+using namespace std::chrono;
+using namespace caf;
+using namespace caf::opencl;
+
+// required to allow sending mem_ref<int> in messages
+namespace caf {
+  template <>
+  struct allowed_unsafe_message_type<mem_ref<uint32_t>> : std::true_type {};
+  template <>
+  struct allowed_unsafe_message_type<opencl::dim_vec> : std::true_type {};
+  template <>
+  struct allowed_unsafe_message_type<spawn_config> : std::true_type {};
+}
+
+namespace {
+
+using uval = uint32_t;
+using uvec = std::vector<uval>;
+using uref = mem_ref<uval>;
+
+/*****************************************************************************\
+                              JUST FOR STUFF
+\*****************************************************************************/
+
+template <class T>
+void valid_or_exit(T expr, const std::string& str = "") {
+  if (expr)
+    return;
+  if (str.empty()) {
+    cout << "[!!!] Something went wrong" << endl;
+  } else {
+    cout << "[!!!] " << str << endl;
+  }
+  exit(-1);
+}
+
+template<class T>
+string as_binary(T num) {
+  stringstream s;
+  auto num_bits = (sizeof(T) * 8);
+  auto added_bits = 0;
+  T mask = T(0x1) << (num_bits - 1);
+  while (mask > 0) {
+    if (added_bits == 32) {
+      s << " ";
+      added_bits = 0;
+    }
+    s << ((num & mask) ? "1" : "0");
+    mask >>= 1;
+    ++added_bits;
+  }
+  return s.str();
+}
+
+template <class T, class E = typename enable_if<is_integral<T>::value>::type>
+T round_up(T numToRound, T multiple)  {
+  assert(multiple > 0);
+  return ((numToRound + multiple - 1) / multiple) * multiple;
+}
+
+template <class T>
+vector<T> segmented_exclusive_scan(const vector<T>& data, const uvec& heads) {
+  assert(data.size() == heads.size());
+  assert(heads[0] == 1);
+  vector<T> results(data.size());
+  for (size_t i = 0; i < data.size(); ++i) {
+    if (heads[i] == 1)
+      results[i] = 0;
+    else
+      results[i] = results[i - 1] + data[i - 1];
+  }
+  return results;
+}
+
+template <class T>
+vector<T> segmented_inclusive_scan(const vector<T>& data, const uvec& heads) {
+  assert(data.size() == heads.size());
+  assert(heads[0] == 1);
+  vector<T> results(data.size());
+  for (size_t i = 0; i < data.size(); ++i) {
+    if (heads[i] == 1)
+      results[i] = data[i];
+    else
+      results[i] = results[i - 1] + data[i];
+  }
+  return results;
+}
+
+/*****************************************************************************\
+                          INTRODUCE SOME CLI ARGUMENTS
+\*****************************************************************************/
+
+class config : public actor_system_config {
+public:
+  size_t iterations = 1000;
+  size_t threshold = 1500;
+  string filename = "";
+  uval bound = 0;
+  string device_name = "GeForce GT 650M";
+  bool print_results;
+  config() {
+    load<opencl::manager>();
+    opt_group{custom_options_, "global"}
+    .add(filename, "data-file,f", "file with test data (one value per line)")
+    .add(bound, "bound,b", "maximum value (0 will scan values)")
+    .add(device_name, "device,d", "device for computation (GeForce GTX 780M, "
+                      "empty string will take first available device)")
+    .add(print_results, "print,p", "print resulting bitmap index")
+    .add(threshold, "threshold,t", "Threshold for output (1500)")
+    .add(iterations, "iterations,i", "Number of times the empty kernel is supposed to run (1000)");
+  }
+};
+
+} // namespace <anonymous>
+
+/*****************************************************************************\
+                                    MAIN!
+\*****************************************************************************/
+
+void caf_main(actor_system& system, const config& cfg) {
+  uvec values;
+  uvec heads;
+  // ---- get data ----
+  if (cfg.filename.empty()) {
+    values.resize(2048);
+    iota(values.begin(), values.end(), 1);
+    // cout << "A filename with test data is required, see --help for detailts."
+    //      << endl;
+    // return;
+  } else {
+    ifstream source{cfg.filename, std::ios::in};
+    uval next;
+    while (source >> next) {
+      values.push_back(next);
+    }
+  }
+  // ---- get device ----
+  auto& mngr = system.opencl_manager();
+  auto opt = mngr.get_device_if([&](const device& dev) {
+      if (cfg.device_name.empty())
+        return true;
+      return dev.get_name() == cfg.device_name;
+  });
+  if (!opt) {
+    opt = mngr.get_device_if([&](const device&) { return true; });
+    if (!opt) {
+      cout << "No device found." << endl;
+      return;
+    }
+    cerr << "Using device '" << opt->get_name() << "'." << endl;
+  }
+
+  // --- scope to ensure actor cleanup ---
+  {
+    // ---- general ----
+    auto dev = *opt;
+    auto prog = mngr.create_program_from_file("./include/segmented_scan.cl",
+                                              "", dev);
+    // ---- input parameters ----
+    size_t n = values.size();
+    size_t group_size = (n + 1) / 2;
+    size_t global_range = round_up((n + 1) / 2, group_size);
+    size_t local_range = group_size;
+    //size_t groups = (global_range / local_range);
+    heads.insert(begin(heads), n, 1);
+    heads[0] = 1;
+    //heads[3] = 1;
+    // ---- ndranges ----
+    //auto ndrange_h = spawn_config{dim_vec{global_range}, {}, dim_vec{local_range}};
+    //auto ndrange_g = spawn_config{dim_vec{round_up(groups, local_range)}, {},
+    //                              dim_vec{local_range}};
+    auto ndrange = spawn_config{dim_vec{global_range}, {},
+                                dim_vec{local_range}};
+
+    // ---- functions for arguments ----
+    /*
+    auto incs = [&](const uvec&, uval n) -> size_t {
+      // calculate number of groups,
+      // depending on the group size from the values size
+      return (round_up((n + 1) / 2,
+              static_cast<uval>(group_size)) / group_size);
+    };
+    */
+
+    // ---- actors ----
+    /*
+    auto phase1 = mngr.spawn_new(prog, "upsweep", ndrange_h,
+                                 in_out<uval, val, mref>{},    // data
+                                 in_out<uval, val, mref>{},    // heads
+                                 out<uval,mref>{incs},         // increments
+                                 out<uval,mref>{incs},         // increment heads
+                                 out<uval,mref>{incs},         // tree
+                                 local<uval>{group_size * 2},  // data buffer
+                                 local<uval>{group_size * 2},  // heads buffer
+                                 priv<uval, val>{});
+    */
+    auto phase2 = mngr.spawn_new(prog, "block_scan", ndrange,   // length / 2 work items
+                                 in_out<uval,val,val>{},        // increments
+                                 in_out<uval,val,val>{},        // increments heads
+                                 //in_out<uval,val,val>{},        // tree
+                                 priv<uval, val>{});            // length
+    /*
+    auto phase3 = mngr.spawn_new(prog, "downsweep", ndrange_h,
+                                 in_out<uval,mref,mref>{},     // data
+                                 in<uval,mref>{},              // heads
+                                 in<uval,mref>{},              // increments
+                                 in<uval,mref>{},              // increment heads
+                                 priv<uval, val>{});
+    */
+
+    // ---- test data ----
+    // TODO: ...
+
+    // ---- computations -----
+    scoped_actor self{system};
+    /*
+    self->send(phase1, values, n);
+    self->receive([&](uref& data, uref& increments) {
+      auto e_data = data.data();
+      auto e_incs = increments.data();
+      auto b = *e_data;
+      auto i = *e_incs;
+      if (b != t_blocks)
+        cout << "Blocks don't match." << endl;
+      if (i != t_increments)
+        cout << "Increments don't match." << endl;
+      self->send(phase2, data, increments, groups);
+    });
+    self->receive([&](uref& data, uref& increments) {
+      self->send(phase3, data, increments, n);
+    });
+    self->receive([&](const uvec& results) {
+      
+    });
+    */
+    self->send(phase2, values, heads, static_cast<uval>(n));
+    auto scanned = segmented_exclusive_scan(values, heads);
+    self->receive([&](uvec& data, uvec&) {
+      if (data != scanned) {
+        cout << "Expected different result" << endl;
+        for (size_t i = 0; i < data.size(); ++i) {
+          //if (scanned[i] != data[i]) {
+            cout << "[" << i << "] " << scanned[i] << " : " << data[i] << endl;
+          //}
+        }
+      } else {
+        cout << "Success" << endl;
+      }
+    });
+  }
+
+  // ---- DONE ----
+  system.await_all_actors_done();
+}
+
+CAF_MAIN()
